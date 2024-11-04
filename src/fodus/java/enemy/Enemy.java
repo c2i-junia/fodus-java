4875eec964c267c7ce2b9aa90aaab717dfd1624c
MatteoPerez <matteo.perez@student.junia.com>
2024-11-04 16:24:34 +0100

    Ajout de l'action defense à enemy

--- src/fodus/java/enemy/Enemy.java
+++ src/fodus/java/enemy/Enemy.java
@@ -2,12 +2,21 @@ package fodus.java.enemy;
 
 import fodus.java.Character;
 import fodus.java.equipments.Equipments;
+import fodus.java.status.*;
 
 public abstract class Enemy extends Character {
     public Equipments drop;
     public abstract void enemyAction(Character target);
+    
     @Override
     public void receiveDamage(int damage){
+        Block defenseStatut = (Block) findTokenType(Block.class);
+        if (defenseStatut != null) {
+            int damageReduction = damage * defenseStatut.getDamageReduction() / 100;
+            damage = damage - damageReduction;
+            System.out.println("Degats reduits de " + defenseStatut.getDamageReduction() + "% !");
+            defenseStatut.updateToken();
+        }
         if(this.healthPoints <= damage){
             this.healthPoints = 0;
         }
