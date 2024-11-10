32e8501327555574f07ca154e75fa9aae9b1ba78
RaphaelMeuniez <raphael.meuniez@student.junia.com>
2024-11-10 18:33:29 +0100

    Update vampire

--- src/fodus/java/enemy/Vampire.java
+++ src/fodus/java/enemy/Vampire.java
@@ -2,7 +2,7 @@ package fodus.java.enemy;
 
 import java.util.Random;
 import fodus.java.Character;
-import fodus.java.equipments.Time_Shield;
+import fodus.java.equipments.TimeShield;
 import fodus.java.status.Block;
 
 public class Vampire extends Enemy{
@@ -16,7 +16,7 @@ public class Vampire extends Enemy{
         this.dexterity = 5;
         this.endurance = 12;
         this.intelligence = 6;
-        this.drop = new Time_Shield();
+        this.drop = new TimeShield();
     }
     @Override
     public void enemyAction(Character target){
